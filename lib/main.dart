99efa1ad881752ccc7ebcd41c18972fe115a7da7
MustafaAswadi <aswadimustafa16@gmail.com>
2022-11-15 23:23:39 +0430

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notify/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: Container(),
+      home: const SplashScreen(),
     );
   }
 }
